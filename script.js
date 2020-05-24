207e01f509521535da3a7024f57098261480a668
RuslanAmraev <beardedsmellyfox@gmail.com>
2020-05-24 13:20:11 +0600

    32

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-function ShowMenu(id){
+function showMenu(id){
 	if(document.getElementById(id).style.display == 'block'){
 		document.getElementById(id).style.display = 'none'
 	}else{
 		document.getElementById(id).style.display = 'block'
+	}
+}
+
+function showMobileMenu(){
+	if(document.getElementById('mobile-menu').style.height == '0px' || document.getElementById('mobile-menu').style.height == 0){
+		document.getElementById('mobile-menu').style.height = '120px'
+	}else{
+		document.getElementById('mobile-menu').style.height = '0px'
 	}
 }
